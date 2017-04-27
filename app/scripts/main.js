942f000cef5a285eaa4309d7a2804eeb1d72645b
leonardo borges <leonardo.borges@cnj.jus.br>
2017-04-27 18:08:48 -0300

    Primeiro commit

---
+++ app/scripts/main.js
@@ -0,0 +1,46 @@
+'use strict';
+
+$(document).ready(function(){
+    
+    //sticky nav
+    $('.navbar').sticky({topSpacing:0});
+    
+    // yamm menu
+    $(document).on('click', '.yamm .dropdown-menu', function(e) {
+        e.stopPropagation();
+    });
+    
+    $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+        event.preventDefault(); 
+        event.stopPropagation(); 
+        $(this).parent().siblings().removeClass('open');
+        $(this).parent().toggleClass('open');
+    });
+    
+    //tootltip
+    $('[data-toggle="tooltip"]').tooltip();
+    
+    //Popover
+    $('[data-toggle="popover"]').popover();
+    
+    
+    
+    //back to top
+    $('body').append('<a href="javascript:void(0);" id="back-to-top"><i class="fa fa-angle-up"></i></a>');
+    
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 200) {
+            $('#back-to-top').fadeIn(200);
+        } else {
+            $('#back-to-top').fadeOut(200);
+        }
+    });
+    $('#back-to-top').click(function() {
+        $('body,html').animate({
+            scrollTop : 0
+        }, 500);
+    });
+        
+    
+    
+});
