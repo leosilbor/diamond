0ba1d13a8c19228c06bdb03ea3cba938417490c2
leonardo.borges <12345678>
2017-04-28 10:52:41 -0300

    Adição de novas páginas

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,7 +3,7 @@
 angular
   .module('diamond', ['ngRoute'])
   
-  .config(function($routeProvider) {
+  .config(function($routeProvider, $locationProvider) {
     $routeProvider
       .when("/", {
           templateUrl : "views/principal.html",
@@ -15,9 +15,17 @@ angular
       .when("/cadastrar", {
           templateUrl : "views/cadastrar.html"
       })
+      .when("/produtos", {
+          templateUrl : "views/produtos.html"
+      })
+      .when("/checkout", {
+          templateUrl : "views/checkout.html"
+      })
       .when("/carrinho", {
           templateUrl : "views/carrinho.html"
       });
+
+      $locationProvider.html5Mode(true);
 }).controller('PrincipalCtrl', function($scope){
 
     
